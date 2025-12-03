56751d9922199899513f33d62e2de9e54ddc74dc
Vordgi <sasha2822222@gmail.com>
2025-12-03 18:09:36 +0000

    nc-11 add support for self-call context

--- package/src/create-server-context.tsx
+++ package/src/create-server-context.tsx
@@ -1,9 +1,9 @@
-import type { ServerContext } from "./types";
-import { AsyncLocalStorage } from "async_hooks";
-import { serverGetterInClientComponentError } from "./server-getter-in-client-component-error";
 import React from "react";
+import { AsyncLocalStorage } from "node:async_hooks";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+import { type ServerContext } from "./types";
+import { serverGetterInClientComponentError } from "./server-getter-in-client-component-error";
+
 const Enter = ({ storage, value }: { storage: AsyncLocalStorage<{ value: unknown }>; value: unknown }) => {
     storage.enterWith({ value });
     return <></>;
@@ -12,25 +12,33 @@ const Enter = ({ storage, value }: { storage: AsyncLocalStorage<{ value: unknown
 function createServerContext<T>(defaultValue?: T): ServerContext<T> {
     serverGetterInClientComponentError("createServerContext");
 
-    const storage: ServerContext<T>["_storage"] = new AsyncLocalStorage<{ value: T }>();
+    const storage: ServerContext<T>["_storage"] = new AsyncLocalStorage<{ value: T | undefined }>();
 
-    return {
-        // The provider must always be asynchronous
-        Provider: async ({ children, value }) => {
-            return (
-                <>
-                    <Enter storage={storage} value={value} />
-                    {children}
-                </>
-            );
-        },
-        Consumer: ({ children }) => {
-            const store = storage.getStore();
-            return children(store ? store.value : defaultValue);
-        },
+    // The provider must always be asynchronous
+    const Provider: ServerContext<T>["Provider"] = async ({ children, value }) => {
+        serverGetterInClientComponentError("ServerContext.Provider");
+        const providerValue = value || defaultValue;
+        return (
+            <>
+                <Enter storage={storage} value={providerValue} />
+                {children}
+            </>
+        );
+    };
+
+    const Consumer: ServerContext<T>["Consumer"] = ({ children }) => {
+        serverGetterInClientComponentError("ServerContext.Consumer");
+        const store = storage.getStore();
+        const consumerValue = store ? store.value : defaultValue;
+        return children(consumerValue);
+    };
+
+    return Object.assign(Provider, {
+        Provider,
+        Consumer,
         _storage: storage,
         _defaultValue: defaultValue,
-    };
+    });
 }
 
 export default createServerContext;
